bfa094141e4f4baf7ecf066587e560462003d592
Martin Honnen <martin.honnen@gmx.de>
2023-03-22 16:13:09 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using net.sf.saxon.s9api;
+using net.liberty_development.SaxonHE12s9apiExtensions;
+
+var processor = new Processor(false);
+
+Console.WriteLine($"{processor.getSaxonEdition()} {processor.getSaxonProductVersion()}");
+
+var xslt30Transformer = processor.newXsltCompiler().Compile(new Uri("https://github.com/martin-honnen/martin-honnen.github.io/raw/master/xslt/processorTestHTML5Xslt3InitialTempl.xsl")).load30();
+
+xslt30Transformer.callTemplate(null, processor.NewSerializer(Console.Out));
